0942d85f42940fe5f01b6cc6fd5194c4168a7f69
Marlou Dev <marlou.dev.app@gmail.com>
2024-06-01 23:08:13 +0800

    added design padding

--- lib/login/views/login_view_form.dart
+++ lib/login/views/login_view_form.dart
@@ -12,42 +12,45 @@ class _LoginViewFormState extends State<LoginViewForm> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset('assets/favicon.png'), // Adding an image
-          const SizedBox(height: 20),
-          const TextField(
-            decoration: InputDecoration(
-              labelText: 'Username',
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/favicon.png'), // Adding an image
+            const SizedBox(height: 20),
+            const TextField(
+              decoration: InputDecoration(
+                labelText: 'Username',
+              ),
             ),
-          ),
-          const SizedBox(height: 20),
-          const TextField(
-            obscureText: true,
-            decoration: InputDecoration(
-              labelText: 'Password',
+            const SizedBox(height: 20),
+            const TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Password',
+              ),
             ),
-          ),
-          const SizedBox(height: 20),
-          SizedBox(
-            width: double.infinity, // Makes the button full width
-            child: ElevatedButton(
-              onPressed: () {
-                context.go('/portal_page');
-              },
-              child: Text('Submit'),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.blue,
-                foregroundColor: Colors.white,
-                shape: const RoundedRectangleBorder(
-                  borderRadius:
-                      BorderRadius.zero, // Makes the button non-rounded
+            const SizedBox(height: 20),
+            SizedBox(
+              width: double.infinity, // Makes the button full width
+              child: ElevatedButton(
+                onPressed: () {
+                  context.go('/portal_page');
+                },
+                child: Text('Submit'),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  foregroundColor: Colors.white,
+                  shape: const RoundedRectangleBorder(
+                    borderRadius:
+                        BorderRadius.zero, // Makes the button non-rounded
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
